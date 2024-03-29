28e009825762ce8e720697934644d81b91ad5087
AIJDprojects <145877755+AIJDprojects@users.noreply.github.com>
2024-03-29 14:54:55 -0500

    Update script.sh

--- Frontend/script.sh
+++ Frontend/script.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+
+# Function to check if a Python package is installed
+is_package_installed() {
+    python -c "import $1" >/dev/null 2>&1
+}
+
+# List of required Python packages
+packages=("uvicorn" "fastapi" "numpy" "scikit-learn" "bs4" "requests" "imgkit")
+
+# Navigate to the directory containing your Python code
 cd Backend
-pip install uvicorn
-pip install fastapi
-pip install numpy
-pip install scikit-learn
-pip install bs4
-pip install requests
-pip install imgkit
+
+# Loop through the list of packages and install if not already installed
+for package in "${packages[@]}"; do
+    if ! is_package_installed "$package"; then
+        echo "Installing $package..."
+        pip install "$package"
+    else
+        echo "$package is already installed."
+    fi
+done
+
