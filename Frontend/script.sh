4dda6cb615d68ff30c731cff6670b7c91484746b
AIJDprojects <145877755+AIJDprojects@users.noreply.github.com>
2024-03-29 15:17:45 -0500

    Update script.sh

--- Frontend/script.sh
+++ Frontend/script.sh
@@ -6,13 +6,13 @@ is_package_installed() {
 }
 
 # List of required Python packages
-packages="uvicorn" "fastapi" "numpy" "scikit-learn" "bs4" "requests" "imgkit"
+packages="uvicorn fastapi numpy scikit-learn bs4 requests imgkit"
 
 # Navigate to the directory containing your Python code
 cd Backend
 
 # Loop through the list of packages and install if not already installed
-for package in "${packages[@]}"; do
+for package in $packages; do
     if ! is_package_installed "$package"; then
         echo "Installing $package..."
         pip install "$package"
